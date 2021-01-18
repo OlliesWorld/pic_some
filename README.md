7b5286fb319f6983b1ba0938d85d44e85e7f8295
Rtlockwood-O <rtlockwoodwork@gmail.com>
2021-01-17 19:41:08 -0700

    complete capstone project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Scrimba Capstone Pic Some App
+
+small ecommerce app to learn how to combine all we have learned.
+Customer can select photos and purchase "prints"
+
+
---
+++ src/Context.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from "react"
+
+const Context = React.createContext()
+
+function ContextProvider({children}) {
+    const [allPhotos, setAllPhotos] = useState([])
+    const [cartItems, setCartItems] = useState([])
+    
+    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAllPhotos(data))
+    }, [])
+    
+    function toggleFav(id) {
+        const updatedArr = allPhotos.map(photo => {
+            if(photo.id === id) {
+                return {...photo, isFavorite: !photo.isFavorite}
+            }
+            return photo
+        })
+        
+        setAllPhotos(updatedArr)
+    }
+    function addToCart(newItems) {
+        setCartItems(prevItems => [...prevItems, newItems])
+    }
+
+    function  removeFromCart(id) {
+        setCartItems(prevItems => prevItems.filter(item => item.id !== id))
+    }
+
+    function emptyCart() {
+        setCartItems([])
+    }
+
+    
+    return (
+        <Context.Provider value={{allPhotos, toggleFav, addToCart, cartItems, removeFromCart, emptyCart}}>
+            {children}
+        </Context.Provider>
+    )
+}
+
+export {ContextProvider, Context}
